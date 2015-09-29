8b34c0e10cb20adabfb388c5118bcdda4c863e69
Ga22be <ga22be@live.se>
2015-09-29 23:48:09 +0200

    Fiddled with the code trying to grasp the assignment

--- XL/src/gui/SlotLabel.java
+++ XL/src/gui/SlotLabel.java
@@ -1,6 +1,8 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class SlotLabel extends ColoredLabel{
     public SlotLabel() {
