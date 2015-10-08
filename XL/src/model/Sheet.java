c97f560fa51fb302d96a9f2794e73d80cfc4a4fc
Pontus Ovhagen <ovhagen@gmail.com>
2015-10-08 11:26:15 +0200

    Second commit of Sheet

--- XL/src/model/Sheet.java
+++ XL/src/model/Sheet.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Observable;
@@ -8,38 +9,40 @@ import model.expr.Environment;
 import model.expr.Expr;
 
 public class Sheet extends Observable implements Environment{
-	private Map<String, Expr> sheet;
-	
+	private Map<String, Slot> sheet;
 	
 	public Sheet(){
-		sheet = new HashMap<String, Expr>();
+		sheet = new HashMap<String, Slot>();
 	}
 	
-	public void save(){
+	public void save(String fileName) throws FileNotFoundException{
+		XLPrintStream printer = new XLPrintStream(fileName);
 	}
 	
-	public void load(String fileName){
+	public void load(String fileName) throws FileNotFoundException{
+		XLBufferedReader reader = new XLBufferedReader(fileName);
+		reader.load(sheet);
 	}
 	
-	public void put(String s, Expr expr){
-		sheet.put(s, expr);
+	public void put(String address, Slot slot){
+		sheet.put(address, slot);
 	}
 	
-	public void replace(String s, Expr expr){
-		sheet.replace(s, expr);
+	public void replace(String address, Slot slot){
+		sheet.replace(address, slot);
 	}
 	
-	public void remove(String s){
-		sheet.remove(s);
+	public void remove(String address){
+		sheet.remove(address);
 	}
 	
-	public Expr getExpr(String s){
-		return sheet.get(s);
+	public Slot getSlot(String address){
+		return sheet.get(address);
 	}
 	
 	@Override
-	public double value(String name) {
-		return 0;
+	public double value(String address) {
+		return sheet.get(address).value(this);
 	}
 
 }
