1b5e765358175c9d930829760b01d6ef4e343dfb
Pnollten <eriksvensson93@hotmail.com>
2015-10-08 10:32:06 +0200

    Försök att fixa mitt/Pontus problem.

--- XL/src/model/Sheet.java
+++ XL/src/model/Sheet.java
@@ -1,8 +1,10 @@
 package model;
 
+import java.util.Observable;
+
 import model.expr.Environment;
 
-public class Sheet implements Environment {
+public class Sheet extends Observable implements Environment {
 
 	@Override
 	public double value(String name) {
