128738981d06a69d9bd5d6a327a358dfb58756a8
Pnollten <eriksvensson93@hotmail.com>
2015-10-10 10:25:50 +0200

    SlotFactory is done!

--- XL/src/model/SlotFactory.java
+++ XL/src/model/SlotFactory.java
@@ -1,9 +1,21 @@
 package model;
 
+import java.io.IOException;
+
+import model.expr.Expr;
+import model.expr.ExprParser;
 
 public class SlotFactory {
 
-	public static Slot generateSlot(String address, String command){
-		return null;
+	public static Slot generateSlot(String command, String address) throws IOException {
+		if (command.startsWith("#")) {
+			return new CommentSlot(new Comment(command.substring(1)), address);
+		} else if (command.contains(address)) {
+			return new CircularSlot(command, address);
+		} else {
+			ExprParser ex = new ExprParser();
+			Expr expr = ex.build(command);
+			return new ExprSlot(expr, address);
+		}
 	}
 }
